ba7ab060e1b59494f0a0a2579bfc27e6d93eb0be
Andrii Guselietov <guseletov@gmail.com>
2021-04-30 09:08:50 +0200

    Correct titels and descriptions

--- test/integration/default/controls/output.rb
+++ test/integration/default/controls/output.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 control "output_check" do
+  title 'Default output check '
+  desc 'Checks that the default output from module existing wiht default value'
 
-
-  describe attribute("output_hello") do
-    it { should exist }
-  end
-  
   describe attribute("output_hello") do
     it { should eq "World" }
   end
